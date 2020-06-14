ce63e2f4cd5a5e7ff6e3f70dc18374f815219f8d
Daniel Alvarez <daniel@alvarez.tech>
2020-06-14 18:27:45 -0400

    Complete about page

--- app/src/main/java/tech/alvarez/facts/about/AboutFragment.kt
+++ app/src/main/java/tech/alvarez/facts/about/AboutFragment.kt
@@ -1,19 +1,21 @@
 package tech.alvarez.facts.about
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import tech.alvarez.facts.R
 import tech.alvarez.facts.databinding.FragmentAboutBinding
 import tech.alvarez.facts.util.Util
+
+const val PROJECT_PAGE = "https://alvarez.tech/projects/facts/"
 
 class AboutFragment : BottomSheetDialogFragment() {
 
     private var _binding: FragmentAboutBinding? = null
-    private val binding
-        get() = _binding!!
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,9 +23,11 @@ class AboutFragment : BottomSheetDialogFragment() {
     ): View? {
         _binding = FragmentAboutBinding.inflate(inflater, container, false)
         with(binding) {
-            titleTextView.text = getString(R.string.app_name)
             versionTextView.text = Util.appVersion()
-            descriptionTextView.text = getString(R.string.app_description)
+            projectButton.setOnClickListener {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(PROJECT_PAGE))
+                startActivity(intent)
+            }
             return root
         }
     }
